a41f13cecb3072827c42afce34c030a4e80e5b01
devi putri <devi.putri@live.com>
2023-10-17 22:52:22 +0700

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+puts "Seeding data 🙈..."
+
+10.times do
+  article = Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraph
+  )
+end
+
+puts "navi"
